1560984981f62af72ebcd6ee3a3a6f4183ab7e28
Daniil <cvb.amaho@gmail.com>
2021-08-09 14:20:11 +0100

    fixUpload

--- app/api/uploadImage.php
+++ app/api/uploadImage.php
@@ -1,10 +1,11 @@
 <?php
 
 if (file_exists($_FILES["image"]["tmp_name"]) && is_uploaded_file($_FILES["image"]["tmp_name"])) {
-	
-
 	$fileExt = explode("/", $_FILES["image"]["type"])[1];
 	$fileName = uniqid() . "." . $fileExt;
+	if (!is_dir("../../assets/img/")) {
+		mkdir("../../assets/img/");
+	}
 	move_uploaded_file($_FILES["image"]["tmp_name"], "../../assets/img/" . $fileName);
 	echo json_encode(array("src" => $fileName));
-}
+} 
